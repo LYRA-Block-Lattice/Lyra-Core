093b4aaf5a3b05fd548123e6b9baa8ccbf335d4f
Wizard Y <rcvbuf@gmail.com>
2019-11-06 12:09:56 +0800

    site revised

---
+++ Mobile/LyraLexWeb/Pages/SendmeLeX.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+
+namespace LyraLexWeb.Pages
+{
+    [BindProperties]
+    public class SendmeLeXModel : PageModel
+    {
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string accountid { get; set; }
+        
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            // write data to txt log
+            if(!string.IsNullOrWhiteSpace(Request.Form["accountid"]))
+            {
+                var log = $"{DateTime.Now}|{Request.Form["userName"]}|{Request.Form["email"]}|{Request.Form["accountid"]}";
+                
+            }
+            // send LeX
+        }
+    }
+}
--- Mobile/LyraLexWeb/Startup.cs
+++ Mobile/LyraLexWeb/Startup.cs
@@ -23,7 +23,7 @@ namespace LyraLexWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,11 +35,12 @@ namespace LyraLexWeb
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            //app.UseHttpsRedirection();  // temperory for dev test. 
+
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -48,9 +49,7 @@ namespace LyraLexWeb
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
