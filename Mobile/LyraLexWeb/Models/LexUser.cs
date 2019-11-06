b34455a96cb4f3c1d9780c579227d5878067e90f
Wizard Y <rcvbuf@gmail.com>
2019-11-06 10:56:43 +0800

    mini fix

---
+++ Mobile/LyraLexWeb/Models/LexUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LyraLexWeb.Models
+{
+    public class LexUser
+    {
+        public int ID;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
