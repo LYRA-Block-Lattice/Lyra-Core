630a40ca6e7208a388b3b21a3d7112b936071eda
Wizard Y <rcvbuf@gmail.com>
2019-11-26 03:23:20 +0800

    project cleanup

---
+++ Client/CommandLine/Core/PreprocessorGuards.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommandLine.Core
+{
+    static class PreprocessorGuards
+    {
+        public static IEnumerable<Func<IEnumerable<string>, IEnumerable<Error>>>
+            Lookup(StringComparer nameComparer, bool autoHelp, bool autoVersion)
+        {
+            var list = new List<Func<IEnumerable<string>, IEnumerable<Error>>>();
+            if (autoHelp)
+                list.Add(HelpCommand(nameComparer));
+            if (autoVersion)
+                list.Add(VersionCommand(nameComparer));
+            return list;
+        }
+
+        public static Func<IEnumerable<string>, IEnumerable<Error>> HelpCommand(StringComparer nameComparer)
+        {
+            return
+                arguments =>
+                    nameComparer.Equals("--help", arguments.First())
+                        ? new Error[] { new HelpRequestedError() }
+                        : Enumerable.Empty<Error>();
+        }
+
+        public static Func<IEnumerable<string>, IEnumerable<Error>> VersionCommand(StringComparer nameComparer)
+        {
+            return
+                arguments =>
+                    nameComparer.Equals("--version", arguments.First())
+                        ? new Error[] { new VersionRequestedError() }
+                        : Enumerable.Empty<Error>();
+        }
+    }
+}
---
+++ Client/CommandLine/Core/TokenPartitioner.cs
@@ -0,0 +1,39 @@
+﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandLine.Infrastructure;
+using CSharpx;
+
+namespace CommandLine.Core
+{
+    static class TokenPartitioner
+    {
+        public static
+            Tuple<IEnumerable<KeyValuePair<string, IEnumerable<string>>>, IEnumerable<string>, IEnumerable<Token>> Partition(
+                IEnumerable<Token> tokens,
+                Func<string, Maybe<TypeDescriptor>> typeLookup)
+        {
+            IEqualityComparer<Token> tokenComparer = ReferenceEqualityComparer.Default;
+
+            var tokenList = tokens.Memorize();
+            var switches = new HashSet<Token>(Switch.Partition(tokenList, typeLookup), tokenComparer);
+            var scalars = new HashSet<Token>(Scalar.Partition(tokenList, typeLookup), tokenComparer);
+            var sequences = new HashSet<Token>(Sequence.Partition(tokenList, typeLookup), tokenComparer);
+            var nonOptions = tokenList
+                .Where(t => !switches.Contains(t))
+                .Where(t => !scalars.Contains(t))
+                .Where(t => !sequences.Contains(t)).Memorize();
+            var values = nonOptions.Where(v => v.IsValue()).Memorize();
+            var errors = nonOptions.Except(values, (IEqualityComparer<Token>)ReferenceEqualityComparer.Default).Memorize();
+
+            return Tuple.Create(
+                    KeyValuePairHelper.ForSwitch(switches)
+                        .Concat(KeyValuePairHelper.ForScalar(scalars))
+                        .Concat(KeyValuePairHelper.ForSequence(sequences)),
+                values.Select(t => t.Text),
+                errors);
+        }
+    }
+}
