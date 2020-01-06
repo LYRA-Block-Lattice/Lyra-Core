00505b308dc130c9314406ea95d5f6698d34eb5a
Wizard Young <rcvbuf@gmail.com>
2020-01-06 18:31:43 +0800

    clean projects

--- Retired/Lyra.Scripts/run-local-node.command
+++
@@ -1,7 +0,0 @@
-﻿#! /bin/bash
-#
-# This script runs local Lyra node ("local" network)
-#
-cd $HOME/Projects/LyraNetwork/Node/Lyra.Node/bin/Debug/netcoreapp2.1/netcoreapp2.1
-dotnet lyranode.dll --networkid local --seed self --database litedb
-
---
+++ Utils/AutoSender/WalletUitl.cs
@@ -0,0 +1,58 @@
+﻿using Lyra.Core.Accounts;
+using Lyra.Core.API;
+using Lyra.Core.Blocks;
+using Lyra.Core.LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSender
+{
+    public class WalletUitl
+    {
+        Wallet wallet;
+        public void OpenWalletFile(string netName, string dataPath, string walletName)
+        {
+            if (wallet != null)
+                throw new Exception("Wallet opening");
+
+            wallet = new Wallet(new LiteAccountDatabase(), netName);
+            wallet.AccountName = walletName;
+            wallet.OpenAccount(dataPath, wallet.AccountName);
+            var AccountID = wallet.AccountId;
+            var PrivateKey = wallet.PrivateKey;
+        }
+
+        public async Task<Dictionary<string, Decimal>> RefreshBalance(string networkId)
+        {
+            var rpcClient = await LyraRestClient.CreateAsync(networkId, "Windows", "AutoSender", "0.1");
+
+            var result = await wallet.Sync(rpcClient);
+            if (result == Lyra.Core.Blocks.APIResultCodes.Success)
+            {
+                return wallet.GetLatestBlock()?.Balances;
+            }
+            else
+            {
+                throw new Exception(result.ToString());
+            }
+        }
+
+        public async Task Transfer(string tokenName, string targetAccount, decimal amount)
+        {
+            //// refresh balance before send. other wise Null Ex
+            //await RefreshBalance();
+            //if (App.Container.Balances[tokenName] < amount)
+            //{
+            //    throw new Exception("Not enough funds for " + tokenName);
+            //}
+
+            var result = await wallet.Send(amount, targetAccount, tokenName);
+            if (result.ResultCode != APIResultCodes.Success)
+            {
+                throw new Exception(result.ToString());
+            }
+        }
+    }
+}
