70fcb702482ab118cf339eff10a42d24a0e2dd1c
wizd <rcvbuf@gmail.com>
2020-01-18 18:04:23 +0800

    fix

--- Core/Lyra.Node2/Startup.cs
+++ Core/Lyra.Node2/Startup.cs
@@ -43,6 +43,7 @@ namespace Lyra.Node2
             services.AddGrpc();
 
             // for consensus
+            services.AddSingleton<DuplexService>();
             services.AddSingleton<IPBFTNet, PBFTNetwork>();
         }
 
