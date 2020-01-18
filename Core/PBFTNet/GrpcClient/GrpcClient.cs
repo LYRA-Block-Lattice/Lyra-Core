a01dd47cebca256d7ed0d404b0ee3f4ac9203299
wizd <rcvbuf@gmail.com>
2020-01-18 21:54:05 +0800

    fix

--- Core/PBFTNet/GrpcClient/GrpcClient.cs
+++ Core/PBFTNet/GrpcClient/GrpcClient.cs
@@ -18,7 +18,7 @@ namespace GrpcClient
             ClientId = accountId;
         }
 
-        public override AsyncDuplexStreamingCall<RequestMessage, ResponseMessage> CreateDuplexClient(ChannelBase channel) =>
+        public override AsyncDuplexStreamingCall<RequestMessage, ResponseMessage> CreateDuplexClient(GrpcChannel channel) =>
             new Messaging.MessagingClient(channel).CreateStreaming();
 
         public void SendObject(object o)
