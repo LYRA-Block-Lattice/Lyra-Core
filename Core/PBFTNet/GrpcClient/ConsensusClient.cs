5ba772647ea1c16dd8f76db9789eb7e74fb99817
wizd <rcvbuf@gmail.com>
2020-01-19 12:17:19 +0800

    fix timeout

--- Core/PBFTNet/GrpcClient/ConsensusClient.cs
+++ Core/PBFTNet/GrpcClient/ConsensusClient.cs
@@ -27,6 +27,7 @@ namespace GrpcClient
             // Return `true` to allow certificates that are untrusted/invalid
             httpClientHandler.ServerCertificateCustomValidationCallback = (a, b, c, d) => true;
             var httpClient = new HttpClient(httpClientHandler);
+            httpClient.Timeout = TimeSpan.FromMinutes(5);
 
             var channelCredentials = new SslCredentials(File.ReadAllText(@"Certs\certificate.crt"));
             //var channel = new Channel($"{nodeAddress}:{PORT}", channelCredentials);
