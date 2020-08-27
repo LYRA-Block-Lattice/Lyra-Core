fba8c776f662dace82142d795e6485be49ba8000
Wizard Young <rcvbuf@gmail.com>
2020-08-28 05:30:49 +0800

    update ver to 1.7.3

--- Client/LyraWallet.Android/MainActivity.cs
+++ Client/LyraWallet.Android/MainActivity.cs
@@ -17,8 +17,8 @@ namespace LyraWallet.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
