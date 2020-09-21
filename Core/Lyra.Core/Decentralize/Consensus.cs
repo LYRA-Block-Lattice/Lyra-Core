364810131e737499441cbbb51bca7c18f765eb50
Wizard Young <rcvbuf@gmail.com>
2020-09-21 22:36:14 +0800

    fix

--- Core/Lyra.Core/Decentralize/Consensus.cs
+++ Core/Lyra.Core/Decentralize/Consensus.cs
@@ -69,7 +69,7 @@ namespace Lyra.Core.Decentralize
                     inq.From = _sys.PosWallet.AccountId;
                     Send2P2pNetwork(inq);
 
-                    await Task.Delay(10000);
+                    await Task.Delay(5000);
                 }
 
                 var q = from ns in _nodeStatus
