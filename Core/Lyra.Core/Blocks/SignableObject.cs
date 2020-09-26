78dad6483ca422f47b5c004449b970af80f2bfa3
Wizard Young <rcvbuf@gmail.com>
2020-09-26 22:31:29 +0800

    fix

--- Core/Lyra.Core/Blocks/SignableObject.cs
+++ Core/Lyra.Core/Blocks/SignableObject.cs
@@ -32,7 +32,7 @@ namespace Lyra.Core.Blocks
                 string hash = Base58Encoding.Encode(hash_bytes);
 
                 // debug only, temp code
-                if(this is TokenGenesisBlock)
+                if(record.Contains("LyraTokenGenesis"))
                     Console.WriteLine($"Hash input: {record}\n Hash: {hash}");
 
                 return hash;
