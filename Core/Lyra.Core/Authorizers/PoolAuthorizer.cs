bb12f9d09854f908d6531a78c8bbf83792a6f1e1
Wizard Young <rcvbuf@gmail.com>
2020-12-31 20:31:23 +0800

    added PoolAuthorizer

---
+++ Core/Lyra.Core/Authorizers/PoolAuthorizer.cs
@@ -0,0 +1,38 @@
+﻿using Lyra.Core.Blocks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyra.Core.Authorizers
+{
+    public class PoolAuthorizer : BaseAuthorizer
+    {
+        public override async Task<(APIResultCodes, AuthorizationSignature)> AuthorizeAsync<T>(DagSystem sys, T tblock)
+        {
+            var result = await AuthorizeImplAsync(sys, tblock);
+
+            if (APIResultCodes.Success == result)
+                return (APIResultCodes.Success, Sign(sys, tblock));
+            else
+                return (result, (AuthorizationSignature)null);
+        }
+        private async Task<APIResultCodes> AuthorizeImplAsync<T>(DagSystem sys, T tblock)
+        {
+            if (!(tblock is PoolBlock))
+                return APIResultCodes.InvalidBlockType;
+
+            var block = tblock as PoolBlock;
+
+            //if (block.AccountID != Neo.ProtocolSettings.Default.StandbyValidators[0])
+            //    return APIResultCodes.InvalidAccountId;
+
+            // Validate blocks
+            var result = await VerifyBlockAsync(sys, block, null);
+            if (result != APIResultCodes.Success)
+                return result;
+
+            return APIResultCodes.Success;
+        }
+    }
+}
