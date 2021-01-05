29a4131c3c11cbf03671735a25e117e401b83dd7
wizard <wizard@DESKTOP-2LOR8VV>
2021-01-05 06:17:29 -0800

    fix

--- Core/Lyra.Data/Crypto/Signatures.cs
+++ Core/Lyra.Data/Crypto/Signatures.cs
@@ -33,7 +33,7 @@ namespace Lyra.Data.Crypto
         {
             try
             {
-                if (AccountId[0] != LyraGlobal.ADDRESSPREFIX)
+                if (AccountId == null || AccountId[0] != LyraGlobal.ADDRESSPREFIX)
                     return false;
 
                 Base58Encoding.DecodeAccountId(AccountId);
