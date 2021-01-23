173bd96b77c232eecdf302f45164e626f2eed174
Wizard Young <rcvbuf@gmail.com>
2021-01-23 23:05:33 +0800

    retry to get ext IP

--- Core/Lyra.Shared/GetPublicIPAddress.cs
+++ Core/Lyra.Shared/GetPublicIPAddress.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Lyra.Shared
 {
@@ -13,28 +14,37 @@ namespace Lyra.Shared
         static string url = "https://api.ipify.org";
         static IPAddress _myIp;
 
-        public static async System.Threading.Tasks.Task<IPAddress> PublicIPAddressAsync(string networkId)
+        public static async Task<IPAddress> PublicIPAddressAsync(string networkId)
         {
             if (_myIp == null)  // no hammer on get ip service.
-                try
+            {
+                int retry = 10;
+                while (retry-- > 0)
                 {
-                    var env = networkId;
-                    if (string.IsNullOrWhiteSpace(env) || env == "devnet")
+                    try
                     {
+                        var env = networkId;
+                        if (string.IsNullOrWhiteSpace(env) || env == "devnet")
+                        {
+                            _myIp = Utilities.LocalIPAddress(false);
+                        }
+                        else
+                        {
+                            var wc = new HttpClient();
+                            var json = await wc.GetStringAsync(url);
+                            _myIp = IPAddress.Parse(json);
+                            break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"In getting IP: {ex.Message}");
                         _myIp = Utilities.LocalIPAddress(false);
-                    }
-                    else
-                    {
-                        var wc = new HttpClient();
-                        var json = await wc.GetStringAsync(url);
-                        _myIp = IPAddress.Parse(json);
+                        await Task.Delay(5000);
                     }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"In getting IP: {ex.Message}");
-                    _myIp = Utilities.LocalIPAddress(false);
-                }
+            }
+
             return _myIp;
         }
     }
