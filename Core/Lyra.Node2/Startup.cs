cc24a71b82b8632526777d6048c12e023ae0ef0a
Wizard Young <rcvbuf@gmail.com>
2021-03-21 03:03:33 +0800

    Revert "reduce logs"
    This reverts commit 42fe94088d253b900ec612fe9d7511c0fcd7eeff.

--- Core/Lyra.Node2/Startup.cs
+++ Core/Lyra.Node2/Startup.cs
@@ -27,8 +27,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using Serilog;
-using Serilog.Events;
 
 namespace Lyra.Node2
 {
@@ -42,17 +40,6 @@ namespace Lyra.Node2
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
-            var logPath = $"{Utilities.GetLyraDataDir(Neo.Settings.Default.LyraNode.Lyra.NetworkId, LyraGlobal.OFFICIALDOMAIN)}/logs/";
-            //loggerFactory.AddFile(logPath + "noded-{Date}.txt");
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
-                .MinimumLevel.Override("System", LogEventLevel.Error)
-                .Enrich.FromLogContext()
-                .WriteTo.File(logPath + "noded-{Date}.txt", rollingInterval: RollingInterval.Day)
-            .CreateLogger();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -178,9 +165,8 @@ namespace Lyra.Node2
                 //app.UseHsts();
             }
 
-            //var logPath = $"{Utilities.GetLyraDataDir(Neo.Settings.Default.LyraNode.Lyra.NetworkId, LyraGlobal.OFFICIALDOMAIN)}/logs/";
-            //loggerFactory.AddFile(logPath + "noded-{Date}.txt");
-            loggerFactory.AddSerilog();
+            var logPath = $"{Utilities.GetLyraDataDir(Neo.Settings.Default.LyraNode.Lyra.NetworkId, LyraGlobal.OFFICIALDOMAIN)}/logs/";
+            loggerFactory.AddFile(logPath + "noded-{Date}.txt");
 
             SimpleLogger.Factory = loggerFactory;
 
