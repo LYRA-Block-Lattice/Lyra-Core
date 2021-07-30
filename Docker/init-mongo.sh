9822d531d9d6ef740c115bd8e864478620feab5f
Wizard Young <rcvbuf@gmail.com>
2021-07-30 21:50:51 +0800

    update mongodb init

--- Docker/init-mongo.sh
+++ Docker/init-mongo.sh
@@ -1,10 +1,12 @@
-mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+#!/usr/bin/env bash
+set -eu
+mongo -- "$MONGO_DB" <<EOF
     var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
     var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
     var admin = db.getSiblingDB('admin');
     admin.auth(rootUser, rootPassword);
 
-    var user = '$MONGO_INITDB_USERNAME';
-    var passwd = '$(cat "$MONGO_INITDB_PASSWORD_FILE")';
+    var user = '$MONGO_USER';
+    var passwd = '${MONGO_PASSWORD}' || user;
     db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
 EOF
