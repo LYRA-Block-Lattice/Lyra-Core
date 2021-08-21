edb837ecc977506e418eab30777d2ed10a742649
Wizard Young <rcvbuf@gmail.com>
2021-08-22 04:03:10 +0800

    reduce log

--- Core/Lyra.Core/Decentralize/ConsensusService.cs
+++ Core/Lyra.Core/Decentralize/ConsensusService.cs
@@ -215,7 +215,7 @@ namespace Lyra.Core.Decentralize
                 {
                     var signedMsg = relayMsg.signedMessage;
 
-                    _log.LogInformation($"ReceiveAsync SignedMessageRelay from {signedMsg.From.Shorten()} Hash {(signedMsg as BlockConsensusMessage)?.BlockHash}");
+                    //_log.LogInformation($"ReceiveAsync SignedMessageRelay from {signedMsg.From.Shorten()} Hash {(signedMsg as BlockConsensusMessage)?.BlockHash}");
 
                     if (signedMsg.TimeStamp < DateTime.UtcNow.AddSeconds(3) &&
                         signedMsg.TimeStamp > DateTime.UtcNow.AddSeconds(-18) &&
