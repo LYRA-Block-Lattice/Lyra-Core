af9c8200ee43b6b7f4688f635862796800237d91
Wizard Young <rcvbuf@gmail.com>
2021-09-06 00:03:43 +0800

    fix

--- Docker/init-mongo-dualnet.sh
+++ Docker/init-mongo-dualnet.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+
+
+echo 'restore database from nebula daily backup...'
+apt-get update \
+  && apt-get install -y wget \
+  && rm -rf /var/lib/apt/lists/*
+wget https://nebula.lyra.live/apps/lyradb-dualnet-daily.tar.gz
+tar -xzf lyradb-dualnet-daily.tar.gz
+mongorestore -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --drop daily
+# rm -rf daily
+# rm -f lyradb-dualnet-daily.tar.gz
+echo 'done restore database.'
+
+
 set -eu
 mongo -- "$MONGO_DB" <<EOF
     var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
@@ -11,15 +25,5 @@ mongo -- "$MONGO_DB" <<EOF
     db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
 EOF
 
-echo 'restore database from nebula daily backup...'
-apt-get update \
-  && apt-get install -y wget \
-  && rm -rf /var/lib/apt/lists/*
-wget https://nebula.lyra.live/apps/lyradb-dualnet-daily.tar.gz
-tar -xzf lyradb-dualnet-daily.tar.gz
-mongorestore -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --drop daily
-# rm -rf daily
-# rm -f lyradb-dualnet-daily.tar.gz
-echo 'done restore database.'
 
 
