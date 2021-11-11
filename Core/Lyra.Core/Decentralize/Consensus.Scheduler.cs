bc42006cb8a252713b24c7b750646376ba3b039d
Wizard Young <rcvbuf@gmail.com>
2021-11-11 23:41:27 +0800

    fix

--- Core/Lyra.Core/Decentralize/Consensus.Scheduler.cs
+++ Core/Lyra.Core/Decentralize/Consensus.Scheduler.cs
@@ -47,17 +47,17 @@ namespace Lyra.Core.Decentralize
             await CreateJobAsync(TimeSpan.FromMinutes(17), typeof(IdleWorks), "Idle Works", jobGroup);
 
             // 10 min view change, 30 min fetch balance.
-            //if (Neo.Settings.Default.LyraNode.Lyra.NetworkId == "devnet")
-            //{
-            //    // need a quick debug test
-            //    await CreateJobAsync("0 0/2 * * * ?", typeof(NewPlayerMonitor), "Player Monitor", jobGroup);
-            //    await CreateJobAsync(TimeSpan.FromMinutes(1), typeof(FetchBalance), "Fetch Balance", jobGroup);
-            //}
-            //else
-            //{
+            if (Neo.Settings.Default.LyraNode.Lyra.NetworkId == "devnet")
+            {
+                // need a quick debug test
+                await CreateJobAsync("0 0/2 * * * ?", typeof(NewPlayerMonitor), "Player Monitor", jobGroup);
+                await CreateJobAsync(TimeSpan.FromMinutes(5), typeof(FetchBalance), "Fetch Balance", jobGroup);
+            }
+            else
+            {
                 await CreateJobAsync("0 0/10 * * * ?", typeof(NewPlayerMonitor), "Player Monitor", jobGroup);
                 await CreateJobAsync(TimeSpan.FromMinutes(60 * 6), typeof(FetchBalance), "Fetch Balance", jobGroup);
-            //}
+            }
 
             // Start up the scheduler (nothing can actually run until the
             // scheduler has been started)
