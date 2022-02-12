a2ab5d5a2ee66006a63de4f50182005dc68dc1c1
Wizard Young <rcvbuf@gmail.com>
2022-02-12 23:37:14 +0800

    ver 3.2.0.3

--- Docker/init-mongo.sh
+++ Docker/init-mongo.sh
@@ -3,18 +3,10 @@ set -eu
 mongo -- "$MONGO_DB" <<EOF
     var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
     var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
-    var db1 = '$MONGO_DB';
-    var db2 = 'Worflow_testnet';
-    var db3 = 'Worflow_mainnet';
     var admin = db.getSiblingDB('admin');
     admin.auth(rootUser, rootPassword);
 
     var user = '$MONGO_USER';
     var passwd = '${MONGO_PASSWORD}';
-    use db1
-    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
-    use db2
-    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
-    use db3
-    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
+    db.createUser({user: user, pwd: passwd, roles: [{ role: "readWrite", db: "lyra" },{ role: "readWrite", db: "Worflow_testnet" },{ role: "readWrite", db: "Worflow_mainnet" }]});
 EOF
