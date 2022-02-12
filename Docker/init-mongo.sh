7fcda34c6014d34a25e7deefd49c40290ce8370d
Wizard Young <rcvbuf@gmail.com>
2022-02-13 00:43:43 +0800

    fix

--- Docker/init-mongo.sh
+++ Docker/init-mongo.sh
@@ -8,5 +8,27 @@ mongo -- "$MONGO_DB" <<EOF
 
     var user = '$MONGO_USER';
     var passwd = '${MONGO_PASSWORD}';
-    admin.createUser({user: user, pwd: passwd, roles: [{ role: "readWrite", db: "lyra" },{ role: "readWrite", db: "Worflow_testnet" },{ role: "readWrite", db: "Worflow_mainnet" }]});
+    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
+EOF
+
+mongo -- "Workflow_testnet" <<EOF
+    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
+
+    var user = '$MONGO_USER';
+    var passwd = '${MONGO_PASSWORD}';
+    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
+EOF
+
+mongo -- "Workflow_mainnet" <<EOF
+    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
+
+    var user = '$MONGO_USER';
+    var passwd = '${MONGO_PASSWORD}';
+    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
 EOF
