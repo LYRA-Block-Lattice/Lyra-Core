0983d4cc95410cab9e3ed09eca18eb81aa7a11c7
wizd <rcvbuf@gmail.com>
2022-08-18 11:24:42 +0800

    update docker image

--- Docker/dbrestore-mainnet.sh
+++ Docker/dbrestore-mainnet.sh
@@ -4,7 +4,7 @@ echo 'restore database from nebula daily backup...'
 apt-get update \
   && apt-get install -y wget \
   && rm -rf /var/lib/apt/lists/*
-wget https://nebula.lyra.live/apps/lyradb-mainnet-daily.tar.gz
+wget https://download.lyra.live/database/lyradb-mainnet-daily.tar.gz
 tar -xzf lyradb-mainnet-daily.tar.gz
 mongorestore -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --drop daily
 rm -rf daily
