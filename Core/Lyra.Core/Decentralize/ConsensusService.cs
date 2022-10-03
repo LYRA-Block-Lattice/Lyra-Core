7a0536d65ce3d71da050f1bc38fc47a5345f83be
wizd <rcvbuf@gmail.com>
2022-10-03 18:26:02 +0800

    fix p2p bug

--- Core/Lyra.Core/Decentralize/ConsensusService.cs
+++ Core/Lyra.Core/Decentralize/ConsensusService.cs
@@ -302,7 +302,7 @@ namespace Lyra.Core.Decentralize
                             // seeds take resp to forward heatbeat, once
                             if ((IsThisNodeSeed && (
                                 signedMsg.MsgType == ChatMessageType.HeartBeat
-                                || (signedMsg is AuthorizingMsg au && (au.Block is ConsolidationBlock || au.Block is ServiceBlock))
+                                || (signedMsg is AuthorizingMsg/* au && (au.Block is ConsolidationBlock || au.Block is ServiceBlock)*/)
                                 //|| signedMsg.MsgType == ChatMessageType.ViewChangeRequest
                                 //|| signedMsg.MsgType == ChatMessageType.ViewChangeReply
                                 //|| signedMsg.MsgType == ChatMessageType.ViewChangeCommit
