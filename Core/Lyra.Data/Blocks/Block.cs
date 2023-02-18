9dff8824953b23d193496953209468e1fa5b1ffd
wizd <rcvbuf@gmail.com>
2023-02-18 09:31:21 +0800

    fix 2

--- Core/Lyra.Data/Blocks/Block.cs
+++ Core/Lyra.Data/Blocks/Block.cs
@@ -12,6 +12,7 @@ using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.Options;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace Lyra.Core.Blocks
 {
@@ -219,21 +220,59 @@ namespace Lyra.Core.Blocks
             }
         }
 
-        public class CustomDecimalConverter : JsonConverter<decimal>
+        class DecimalJsonConverter : JsonConverter
         {
-            public override void WriteJson(JsonWriter writer, decimal value, Newtonsoft.Json.JsonSerializer serializer)
+            public DecimalJsonConverter()
             {
-                writer.WriteRawValue(value.ToString("g"));
             }
 
-            public override decimal ReadJson(JsonReader reader, Type objectType, decimal existingValue, bool hasExistingValue, Newtonsoft.Json.JsonSerializer serializer)
+            public override bool CanRead
             {
-                if (reader.Value == null)
+                get
                 {
-                    return 0m;
+                    return false;
+                }
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                throw new NotImplementedException("Unnecessary because CanRead is false. The type will skip the converter.");
+            }
+
+            public override bool CanConvert(Type objectType)
+            {
+                return (objectType == typeof(decimal) || objectType == typeof(float) || objectType == typeof(double));
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                if (DecimalJsonConverter.IsWholeValue(value))
+                {
+                    writer.WriteRawValue(JsonConvert.ToString(Convert.ToInt64(value)));
+                }
+                else
+                {
+                    writer.WriteRawValue(JsonConvert.ToString(value));
+                }
+            }
+
+            private static bool IsWholeValue(object value)
+            {
+                if (value is decimal decimalValue)
+                {
+                    int precision = (Decimal.GetBits(decimalValue)[3] >> 16) & 0x000000FF;
+                    return precision == 0;
+                }
+                else if (value is float floatValue)
+                {
+                    return floatValue == Math.Truncate(floatValue);
+                }
+                else if (value is double doubleValue)
+                {
+                    return doubleValue == Math.Truncate(doubleValue);
                 }
 
-                return Convert.ToDecimal(reader.Value);
+                return false;
             }
         }
 
@@ -246,7 +285,7 @@ namespace Lyra.Core.Blocks
                     ContractResolver = new CustomResolver(new List<string> { "Hash", "Signature" }),
                     StringEscapeHandling = StringEscapeHandling.EscapeNonAscii,
                     DateFormatString = "yyyy-MM-ddTHH:mm:ss.fffZ",
-                    Converters= new List<JsonConverter> { new CustomDecimalConverter() },                    
+                    Converters= new List<JsonConverter> { new DecimalJsonConverter() },                    
                 };
 
                 if (this is TransactionBlock tx)
